283d170d4713e07f2a17a3fa853c4a567608931f
naqashafzal <naqash2@live.com>
2026-08-28 13:29:47 +0500

    modified:   next.config.ts renamed:    src/middleware.ts -> src/proxy.ts

--- next.config.ts
+++ next.config.ts
@@ -26,11 +26,7 @@ const nextConfig: NextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+
   async redirects() {
     return [
       {
